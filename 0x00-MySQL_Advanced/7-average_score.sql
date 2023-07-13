4d7be2b2cb40885a2e8821022fdd661a0e799b81
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-13 18:00:12 +0100

    Task 07

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,19 @@
+-- 7. Average score
+--  computes and store the average score for a student
+
+DELIMITER $$ ;
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+    )
+    BEGIN
+        UPDATE users
+        SET average_score = (
+            SELECT SUM(score) / COUNT(user_id)
+            FROM corrections
+            WHERE user_id = user_id
+        )
+        WHERE id = user_id;
+    END $$
+
+DELIMITER ; $$
