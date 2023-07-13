99b6aad19b6008da783c1b6dea941689e6ce7b88
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-13 18:59:41 +0100

    Task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- 11. No table for a meeting
+-- lists all students that have a score under 80 (strict)
+-- and no last_meeting or more than 1 month.
+
+CREATE OR REPLACE VIEW need_meeting AS
+SELECT name from students
+WHERE score < 80
+    AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
