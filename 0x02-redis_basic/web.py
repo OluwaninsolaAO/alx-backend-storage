9e430ca2082cca9564cf2509567e367d7bb328a1
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-19 14:45:05 +0100

    Task 100

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""5. Implementing an expiring web cache and tracker"""
+
+from requests import get
+import redis
+from functools import wraps
+from typing import Callable
+
+
+def track_page_count(method: Callable) -> Callable:
+    """Tracks how many times a particular
+    URL was accessed"""
+
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """Inner Wrapper Function"""
+        cache = redis.Redis()
+        url = args[0]
+        key = 'count:{' + url + '}'
+        cache.incr(key)
+        page = cache.get(url)
+        if page is not None:
+            return page.encode('utf-8')
+        page = method(*args, **kwargs)
+        cache.set(key, page, 10)
+        return page
+    return wrapper
+
+
+def get_page(url: str) -> str:
+    """Obtain the HTML content of a particular
+    URL and returns it."""
+    return get(url).text
