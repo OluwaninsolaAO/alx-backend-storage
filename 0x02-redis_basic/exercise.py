fd1e9ed39ba64594162414a81e24bda6b334e2c8
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-19 14:05:04 +0100

    Task 04

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,7 +20,7 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """store the history of inputs and outputs for 
+    """store the history of inputs and outputs for
     a particular function"""
     in_key = method.__qualname__ + ':inputs'
     out_key = method.__qualname__ + ':outputs'
